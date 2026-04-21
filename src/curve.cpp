31be87625804a22360d5f89565645cb8d1651b7c
oier1kzc <2723875943@qq.com>
2026-04-21 21:54:32 +0800

    kzccommit

--- src/curve.cpp
+++ src/curve.cpp
@@ -1,5 +1,6 @@
 #include "curve.h"
 #include "vertexrecorder.h"
+#include <bits/stdc++.h>
 using namespace std;
 
 const float c_pi = 3.14159265358979323846f;
@@ -8,147 +9,206 @@ namespace {
 // Approximately equal to.  We don't want to use == because of
 // precision issues with floating point.
 inline bool approx(const Vector3f &lhs, const Vector3f &rhs) {
-    const float eps = 1e-8f;
-    return (lhs - rhs).absSquared() < eps;
+	const float eps = 1e-8f;
+	return (lhs - rhs).absSquared() < eps;
 }
 
 } // namespace
 
+constexpr float cube(float t) { return t * t * t; }
+
+template <class T>
+constexpr T operator * (const array<T, 4> &e, const Vector4f &v) {
+	return v.x() * e[0] + v.y() * e[1] + v.z() * e[2] + v.w() * e[3];
+}
+
 Curve evalBezier(const vector<Vector3f> &P, unsigned steps) {
-    // Check
-    if (P.size() < 4 || P.size() % 3 != 1) {
-        cerr << "evalBezier must be called with 3n+1 control points." << endl;
-        exit(0);
-    }
+	// Check
+	if (P.size() < 4 || P.size() % 3 != 1) {
+		cerr << "evalBezier must be called with 3n+1 control points." << endl;
+		exit(0);
+	}
 
-    // TODO:
-    // You should implement this function so that it returns a Curve
-    // (e.g., a vector< CurvePoint >).  The variable "steps" tells you
-    // the number of points to generate on each piece of the spline.
-    // At least, that's how the sample solution is implemented and how
-    // the SWP files are written.  But you are free to interpret this
-    // variable however you want, so long as you can control the
-    // "resolution" of the discretized spline curve with it.
+	// TODO:
+	// You should implement this function so that it returns a Curve
+	// (e.g., a vector< CurvePoint >).  The variable "steps" tells you
+	// the number of points to generate on each piece of the spline.
+	// At least, that's how the sample solution is implemented and how
+	// the SWP files are written.  But you are free to interpret this
+	// variable however you want, so long as you can control the
+	// "resolution" of the discretized spline curve with it.
 
-    // Make sure that this function computes all the appropriate
-    // Vector3fs for each CurvePoint: V,T,N,B.
-    // [NBT] should be unit and orthogonal.
+	// Make sure that this function computes all the appropriate
+	// Vector3fs for each CurvePoint: V,T,N,B.
+	// [NBT] should be unit and orthogonal.
 
-    // Also note that you may assume that all Bezier curves that you
-    // receive have G1 continuity.  Otherwise, the TNB will not be
-    // be defined at points where this does not hold.
+	// Also note that you may assume that all Bezier curves that you
+	// receive have G1 continuity.  Otherwise, the TNB will not be
+	// be defined at points where this does not hold.
 
-    cerr << "\t>>> evalBezier has been called with the following input:"
-         << endl;
+	cerr << "\t>>> evalBezier has been called with the following input:" << endl;
 
-    cerr << "\t>>> Control points (type vector< Vector3f >): " << endl;
-    for (int i = 0; i < (int)P.size(); ++i) {
-        cerr << "\t>>> " << P[i] << endl;
-    }
+	cerr << "\t>>> Control points (type vector< Vector3f >): " << endl;
+	for (int i = 0; i < (int)P.size(); ++i) {
+		cerr << "\t>>> " << P[i] << endl;
+	}
 
-    cerr << "\t>>> Steps (type steps): " << steps << endl;
-    cerr << "\t>>> Returning empty curve." << endl;
+	cerr << "\t>>> Steps (type steps): " << steps << endl;
 
-    // Right now this will just return this empty curve.
-    return Curve();
+	return evalBspline(P, steps);
+
+	// return Curve();
+
+	assert((int)P.size() == 4);
+
+	const array<Vector3f, 4> P0 = {P[0], P[1], P[2], P[4]};
+
+	static const Matrix4f Mbez(
+		1, -3, 3, -1,
+		0, 3, -6, 3,
+		0, 0, 3, -3,
+		0, 0, 0, 1
+	);
+
+	Curve R(steps + 1);
+	for (unsigned i = 0; i <= steps; ++i) {
+		float t = i / (float)steps;
+		Vector4f Vt(1, t, t * t, t * t * t);
+		Vector4f Tt(0, 1, 2 * t, 3 * t * t);
+		Vector4f Nt(0, 0, 2, 6 * t);
+		R[i].V = P0 * (Mbez * Vt);
+		R[i].T = P0 * (Mbez * Tt);
+		R[i].N = P0 * (Mbez * Nt);
+		R[i].B = Vector3f(0, 0, 1);
+	}
+
+	return R;
 }
 
 Curve evalBspline(const vector<Vector3f> &P, unsigned steps) {
-    // Check
-    if (P.size() < 4) {
-        cerr << "evalBspline must be called with 4 or more control points."
-             << endl;
-        exit(0);
-    }
+	// Check
+	if (P.size() < 4) {
+		cerr << "evalBspline must be called with 4 or more control points." << endl;
+		exit(0);
+	}
 
-    // TODO:
-    // It is suggested that you implement this function by changing
-    // basis from B-spline to Bezier.  That way, you can just call
-    // your evalBezier function.
+	// TODO:
+	// It is suggested that you implement this function by changing
+	// basis from B-spline to Bezier.  That way, you can just call
+	// your evalBezier function.
 
-    cerr << "\t>>> evalBSpline has been called with the following input:"
-         << endl;
+	cerr << "\t>>> evalBSpline has been called with the following input:"
+		 << endl;
 
-    cerr << "\t>>> Control points (type vector< Vector3f >): " << endl;
-    for (int i = 0; i < (int)P.size(); ++i) {
-        cerr << "\t>>> " << P[i] << endl;
-    }
+	cerr << "\t>>> Control points (type vector< Vector3f >): " << endl;
+	for (int i = 0; i < (int)P.size(); ++i) {
+		cerr << "\t>>> " << P[i].x() << " " << P[i].y() << " " << P[i].z() << endl;
+	}
+	cerr << "\t>>> Steps (type steps): " << steps << endl;
 
-    cerr << "\t>>> Steps (type steps): " << steps << endl;
-    cerr << "\t>>> Returning empty curve." << endl;
+	static const Matrix4f Mb(
+		1, -3, 3, -1,
+		4, 0, -6, 3,
+		1, 3, 3, -3,
+		0, 0, 0, 1
+	);
+	constexpr float C = 6;
 
-    // Return an empty curve right now.
-    return Curve();
+	Curve R(steps * (P.size() - 3) + 1);
+	for (size_t j = 0; j + 3 < P.size(); ++j) {
+		const array<Vector3f, 4> Pj = {P[j], P[j + 1], P[j + 2], P[j + 3]};
+		for (unsigned i = 0, k = j * steps; i < steps; ++i, ++k) {
+			float t = i / (float)steps;
+			Vector4f Vt(1, t, t * t, t * t * t);
+			R[k].V = Pj * (Mb * Vt / C);
+			Vector4f Tt(0, 1, 2 * t, 3 * t * t);
+			R[k].T = (Pj * (Mb * Tt / C)).normalized();
+			/* Vector4f Nt(0, 0, 2, 6 * t);
+			R[k].N = Pj * (Mb * Nt / C);
+			R[k].N.normalize();
+			R[k].B = Vector3f(0, 0, 1); */
+		}
+	}
+
+	R.back() = R[0];
+	
+	R[0].B = Vector3f(0, 0, 1);
+	for (size_t i = 1; i < R.size(); ++i) {
+		R[i].N = Vector3f::cross(R[i - 1].B, R[i].T).normalized();
+		R[i].B = Vector3f::cross(R[i].T, R[i].N).normalized();
+	}
+
+	return R;
 }
 
 Curve evalCircle(float radius, unsigned steps) {
-    // This is a sample function on how to properly initialize a Curve
-    // (which is a vector< CurvePoint >).
+	// This is a sample function on how to properly initialize a Curve
+	// (which is a vector< CurvePoint >).
 
-    // Preallocate a curve with steps+1 CurvePoints
-    Curve R(steps + 1);
+	// Preallocate a curve with steps+1 CurvePoints
+	Curve R(steps + 1);
 
-    // Fill it in counterclockwise
-    for (unsigned i = 0; i <= steps; ++i) {
-        // step from 0 to 2pi
-        float t = 2.0f * c_pi * float(i) / steps;
+	// Fill it in counterclockwise
+	for (unsigned i = 0; i <= steps; ++i) {
+		// step from 0 to 2pi
+		float t = 2.0f * c_pi * float(i) / steps;
 
-        // Initialize position
-        // We're pivoting counterclockwise around the y-axis
-        R[i].V = radius * Vector3f(cos(t), sin(t), 0);
+		// Initialize position
+		// We're pivoting counterclockwise around the y-axis
+		R[i].V = radius * Vector3f(cos(t), sin(t), 0);
 
-        // Tangent vector is first derivative
-        R[i].T = Vector3f(-sin(t), cos(t), 0);
+		// Tangent vector is first derivative
+		R[i].T = Vector3f(-sin(t), cos(t), 0);
 
-        // Normal vector is second derivative
-        R[i].N = Vector3f(-cos(t), -sin(t), 0);
+		// Normal vector is second derivative
+		R[i].N = Vector3f(-cos(t), -sin(t), 0);
 
-        // Finally, binormal is facing up.
-        R[i].B = Vector3f(0, 0, 1);
-    }
+		// Finally, binormal is facing up.
+		R[i].B = Vector3f(0, 0, 1);
+	}
 
-    return R;
+	return R;
 }
 
 void recordCurve(const Curve &curve, VertexRecorder *recorder) {
-    const Vector3f WHITE(1, 1, 1);
-    for (int i = 0; i < (int)curve.size() - 1; ++i) {
-        recorder->record_poscolor(curve[i].V, WHITE);
-        recorder->record_poscolor(curve[i + 1].V, WHITE);
-    }
+	const Vector3f WHITE(1, 1, 1);
+	for (int i = 0; i < (int)curve.size() - 1; ++i) {
+		recorder->record_poscolor(curve[i].V, WHITE);
+		recorder->record_poscolor(curve[i + 1].V, WHITE);
+	}
 }
 void recordCurveFrames(const Curve &curve, VertexRecorder *recorder,
-                       float framesize) {
-    Matrix4f T;
-    const Vector3f RED(1, 0, 0);
-    const Vector3f GREEN(0, 1, 0);
-    const Vector3f BLUE(0, 0, 1);
+					   float framesize) {
+	Matrix4f T;
+	const Vector3f RED(1, 0, 0);
+	const Vector3f GREEN(0, 1, 0);
+	const Vector3f BLUE(0, 0, 1);
 
-    const Vector4f ORGN(0, 0, 0, 1);
-    const Vector4f AXISX(framesize, 0, 0, 1);
-    const Vector4f AXISY(0, framesize, 0, 1);
-    const Vector4f AXISZ(0, 0, framesize, 1);
+	const Vector4f ORGN(0, 0, 0, 1);
+	const Vector4f AXISX(framesize, 0, 0, 1);
+	const Vector4f AXISY(0, framesize, 0, 1);
+	const Vector4f AXISZ(0, 0, framesize, 1);
 
-    for (int i = 0; i < (int)curve.size(); ++i) {
-        T.setCol(0, Vector4f(curve[i].N, 0));
-        T.setCol(1, Vector4f(curve[i].B, 0));
-        T.setCol(2, Vector4f(curve[i].T, 0));
-        T.setCol(3, Vector4f(curve[i].V, 1));
+	for (int i = 0; i < (int)curve.size(); ++i) {
+		T.setCol(0, Vector4f(curve[i].N, 0));
+		T.setCol(1, Vector4f(curve[i].B, 0));
+		T.setCol(2, Vector4f(curve[i].T, 0));
+		T.setCol(3, Vector4f(curve[i].V, 1));
 
-        // Transform orthogonal frames into model space
-        Vector4f MORGN = T * ORGN;
-        Vector4f MAXISX = T * AXISX;
-        Vector4f MAXISY = T * AXISY;
-        Vector4f MAXISZ = T * AXISZ;
+		// Transform orthogonal frames into model space
+		Vector4f MORGN = T * ORGN;
+		Vector4f MAXISX = T * AXISX;
+		Vector4f MAXISY = T * AXISY;
+		Vector4f MAXISZ = T * AXISZ;
 
-        // Record in model space
-        recorder->record_poscolor(MORGN.xyz(), RED);
-        recorder->record_poscolor(MAXISX.xyz(), RED);
+		// Record in model space
+		recorder->record_poscolor(MORGN.xyz(), RED);
+		recorder->record_poscolor(MAXISX.xyz(), RED);
 
-        recorder->record_poscolor(MORGN.xyz(), GREEN);
-        recorder->record_poscolor(MAXISY.xyz(), GREEN);
+		recorder->record_poscolor(MORGN.xyz(), GREEN);
+		recorder->record_poscolor(MAXISY.xyz(), GREEN);
 
-        recorder->record_poscolor(MORGN.xyz(), BLUE);
-        recorder->record_poscolor(MAXISZ.xyz(), BLUE);
-    }
+		recorder->record_poscolor(MORGN.xyz(), BLUE);
+		recorder->record_poscolor(MAXISZ.xyz(), BLUE);
+	}
 }
--- src/surf.cpp
+++ src/surf.cpp
@@ -1,5 +1,6 @@
 #include "surf.h"
 #include "vertexrecorder.h"
+#include <bits/stdc++.h>
 using namespace std;
 
 namespace {
@@ -7,109 +8,145 @@ namespace {
 // We're only implenting swept surfaces where the profile curve is
 // flat on the xy-plane.  This is a check function.
 static bool checkFlat(const Curve &profile) {
-    for (unsigned i = 0; i < profile.size(); i++)
-        if (profile[i].V[2] != 0.0 || profile[i].T[2] != 0.0 ||
-            profile[i].N[2] != 0.0)
-            return false;
+	for (unsigned i = 0; i < profile.size(); i++)
+		if (profile[i].V[2] != 0.0 || profile[i].T[2] != 0.0 ||
+			profile[i].N[2] != 0.0)
+			return false;
 
-    return true;
+	return true;
 }
 } // namespace
 
 // DEBUG HELPER
 Surface quad() {
-    Surface ret;
-    ret.VV.push_back(Vector3f(-1, -1, 0));
-    ret.VV.push_back(Vector3f(+1, -1, 0));
-    ret.VV.push_back(Vector3f(+1, +1, 0));
-    ret.VV.push_back(Vector3f(-1, +1, 0));
+	Surface ret;
+	ret.VV.push_back(Vector3f(-1, -1, 0));
+	ret.VV.push_back(Vector3f(+1, -1, 0));
+	ret.VV.push_back(Vector3f(+1, +1, 0));
+	ret.VV.push_back(Vector3f(-1, +1, 0));
 
-    ret.VN.push_back(Vector3f(0, 0, 1));
-    ret.VN.push_back(Vector3f(0, 0, 1));
-    ret.VN.push_back(Vector3f(0, 0, 1));
-    ret.VN.push_back(Vector3f(0, 0, 1));
+	ret.VN.push_back(Vector3f(0, 0, 1));
+	ret.VN.push_back(Vector3f(0, 0, 1));
+	ret.VN.push_back(Vector3f(0, 0, 1));
+	ret.VN.push_back(Vector3f(0, 0, 1));
 
-    ret.VF.push_back(Tup3u(0, 1, 2));
-    ret.VF.push_back(Tup3u(0, 2, 3));
-    return ret;
+	ret.VF.push_back(Tup3u(0, 1, 2));
+	ret.VF.push_back(Tup3u(0, 2, 3));
+	return ret;
+}
+
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+
+Vector3f rndVector3f() {
+	uniform_real_distribution<> rng(-1, 1);
+	Vector3f e(rng(rnd), rng(rnd), rng(rnd));
+	e.normalize();
+	return e;
 }
 
 Surface makeSurfRev(const Curve &profile, unsigned steps) {
-    Surface surface;
-    surface = quad();
+	if (!checkFlat(profile)) {
+		cerr << "surfRev profile curve must be flat on xy plane." << endl;
+		exit(0);
+	}
 
-    if (!checkFlat(profile)) {
-        cerr << "surfRev profile curve must be flat on xy plane." << endl;
-        exit(0);
-    }
+	// TODO: Here you should build the surface.  See surf.h for details.
 
-    // TODO: Here you should build the surface.  See surf.h for details.
+	cerr << "\t>>> makeSurfRev called\n";
+	cerr << "\t>>> Steps (type steps): " << steps << endl;
 
-    cerr << "\t>>> makeSurfRev called (but not implemented).\n\t>>> Returning "
-            "empty surface."
-         << endl;
+	// return quad();
 
-    return surface;
+	Surface surface;
+	
+	static constexpr float PI = 3.14159265358979323846f;
+	
+	for (unsigned i = 0, k = 0; i <= steps; ++i) {
+		// step from 0 to 2pi
+		float t = 2.0f * PI * float(i) / steps;
+		float sinT = sin(t), cosT = cos(t);
+
+		// Initialize position
+		// We're pivoting counterclockwise around the y-axis
+		
+		for (size_t j = 0; j < profile.size(); ++j, ++k) {
+			auto &[V, T, N, B] = profile[j];
+			surface.VV.push_back(Vector3f(V.x() * cosT, V.y(), V.x() * sinT));
+			// surface.VN.push_back(Vector3f(T.x() * cosT, T.y(), T.x() * sinT));
+			surface.VN.push_back(Vector3f(-N.x() * cosT, -N.y(), -N.x() * sinT));
+			// surface.VN.push_back(rndVector3f());
+			if (i && j) {
+				surface.VF.push_back(Tup3u(k - (unsigned)profile.size() - 1, k - 1, k));
+				surface.VF.push_back(Tup3u(k - (unsigned)profile.size() - 1, k, k - (unsigned)profile.size()));
+				/* surface.VF.push_back(Tup3u(k - (unsigned)profile.size() - 1, k - (unsigned)profile.size(), k - 1));
+				surface.VF.push_back(Tup3u(k - (unsigned)profile.size(), k - 1, k)); */
+			}
+		}
+	}
+
+	cerr << "\t>>> " << surface.VV.size() << "\n";
+
+	return surface;
 }
 
 Surface makeGenCyl(const Curve &profile, const Curve &sweep) {
-    Surface surface;
-    surface = quad();
+	Surface surface;
+	surface = quad();
 
-    if (!checkFlat(profile)) {
-        cerr << "genCyl profile curve must be flat on xy plane." << endl;
-        exit(0);
-    }
+	if (!checkFlat(profile)) {
+		cerr << "genCyl profile curve must be flat on xy plane." << endl;
+		exit(0);
+	}
 
-    // TODO: Here you should build the surface.  See surf.h for details.
+	// TODO: Here you should build the surface.  See surf.h for details.
 
-    cerr << "\t>>> makeGenCyl called (but not implemented).\n\t>>> Returning "
-            "empty surface."
-         << endl;
+	cerr << "\t>>> makeGenCyl called (but not implemented).\n\t>>> Returning "
+			"empty surface."
+		 << endl;
 
-    return surface;
+	return surface;
 }
 
 void recordSurface(const Surface &surface, VertexRecorder *recorder) {
-    const Vector3f WIRECOLOR(0.4f, 0.4f, 0.4f);
-    for (int i = 0; i < (int)surface.VF.size(); i++) {
-        recorder->record(surface.VV[surface.VF[i][0]],
-                         surface.VN[surface.VF[i][0]], WIRECOLOR);
-        recorder->record(surface.VV[surface.VF[i][1]],
-                         surface.VN[surface.VF[i][1]], WIRECOLOR);
-        recorder->record(surface.VV[surface.VF[i][2]],
-                         surface.VN[surface.VF[i][2]], WIRECOLOR);
-    }
+	const Vector3f WIRECOLOR(0.4f, 0.4f, 0.4f);
+	for (int i = 0; i < (int)surface.VF.size(); i++) {
+		recorder->record(surface.VV[surface.VF[i][0]],
+						 surface.VN[surface.VF[i][0]], WIRECOLOR);
+		recorder->record(surface.VV[surface.VF[i][1]],
+						 surface.VN[surface.VF[i][1]], WIRECOLOR);
+		recorder->record(surface.VV[surface.VF[i][2]],
+						 surface.VN[surface.VF[i][2]], WIRECOLOR);
+	}
 }
 
 void recordNormals(const Surface &surface, VertexRecorder *recorder,
-                   float len) {
-    const Vector3f NORMALCOLOR(0, 1, 1);
-    for (int i = 0; i < (int)surface.VV.size(); i++) {
-        recorder->record_poscolor(surface.VV[i], NORMALCOLOR);
-        recorder->record_poscolor(surface.VV[i] + surface.VN[i] * len,
-                                  NORMALCOLOR);
-    }
+				   float len) {
+	const Vector3f NORMALCOLOR(0, 1, 1);
+	for (int i = 0; i < (int)surface.VV.size(); i++) {
+		recorder->record_poscolor(surface.VV[i], NORMALCOLOR);
+		recorder->record_poscolor(surface.VV[i] + surface.VN[i] * len,
+								  NORMALCOLOR);
+	}
 }
 
 void outputObjFile(ostream &out, const Surface &surface) {
 
-    for (int i = 0; i < (int)surface.VV.size(); i++)
-        out << "v  " << surface.VV[i][0] << " " << surface.VV[i][1] << " "
-            << surface.VV[i][2] << endl;
+	for (int i = 0; i < (int)surface.VV.size(); i++)
+		out << "v  " << surface.VV[i][0] << " " << surface.VV[i][1] << " "
+			<< surface.VV[i][2] << endl;
 
-    for (int i = 0; i < (int)surface.VN.size(); i++)
-        out << "vn " << surface.VN[i][0] << " " << surface.VN[i][1] << " "
-            << surface.VN[i][2] << endl;
+	for (int i = 0; i < (int)surface.VN.size(); i++)
+		out << "vn " << surface.VN[i][0] << " " << surface.VN[i][1] << " "
+			<< surface.VN[i][2] << endl;
 
-    out << "vt  0 0 0" << endl;
+	out << "vt  0 0 0" << endl;
 
-    for (int i = 0; i < (int)surface.VF.size(); i++) {
-        out << "f  ";
-        for (unsigned j = 0; j < 3; j++) {
-            unsigned a = surface.VF[i][j] + 1;
-            out << a << "/" << "1" << "/" << a << " ";
-        }
-        out << endl;
-    }
+	for (int i = 0; i < (int)surface.VF.size(); i++) {
+		out << "f  ";
+		for (unsigned j = 0; j < 3; j++) {
+			unsigned a = surface.VF[i][j] + 1;
+			out << a << "/" << "1" << "/" << a << " ";
+		}
+		out << endl;
+	}
 }
